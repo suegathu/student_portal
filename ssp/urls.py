6b4d72db8d7b975ba110b2fe982ce6e713989d13
Suegathh <suzzainnegathu@gmail.com>
2025-02-27 18:42:55 +0300

    add static url

--- ssp/urls.py
+++ ssp/urls.py
@@ -27,4 +27,24 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='dashboard/logout.html'), name='logout'),
 
     path('profile/', dash_views.profile, name='profile'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from dashboard import views as dash_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),
+    path('register/', dash_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # LogoutView doesn't need a template
+
+    path('profile/', dash_views.profile, name='profile'),
 ]
+
+# Serving media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
